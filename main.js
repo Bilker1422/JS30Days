84a3c4652f20f074145ea06f9866b8ec6a1a0c2c
Bilker1422 <Bilker1422@gmail.com>
2022-06-20 11:19:46 +0300

    done 1days

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('Hello world');
+
+function msg() {
+    alert("You just Clicked me!");
+    alert("That hurt!");
+    alert("YOOOO!!!, I told  it's Hurt a lot stop!");
+    alert("Wp u got hacked, hahahahah");
+    //comments can make code readable
+    //Welcome to 30DaysOfJavaScript
+    /*
+    comments can make code readable, easy to reuse and informative
+     */
+}
